e0fa9a78d6714298828ec3eb398486fe06420ec5
sipuli93 <66952910+sipuli93@users.noreply.github.com>
2021-04-11 19:15:16 +0300

    Update MMM-SensorTemps.js

--- MMM-SensorTemps.js
+++ MMM-SensorTemps.js
@@ -13,25 +13,34 @@ Module.register("MMM-IndoorAndSaunaTemp", {
 		saunaReadyLimit: 60,
 		indoorSensorHeader: "Indoor",
 		saunaSensorHeader: "Sauna",
+		initialLoadDelay: 0,
+		retryDelay: 10 * 1000, //10sec
+		updateInterval: 60 * 1000, //1min
+		animationSpeed: 1000
 	},
 
 	requiresVersion: "2.1.0", // Required version of MagicMirror
 
 	start: function() {
-		this.indoorSensor = {
-			MAC: this.config.indoorSensorMAC,
-			temp: NaN,
-			humidity: NaN,
-			header: this.config.indoorSensorHeader
+		Log.info("Starting module: " + this.name);
+		
+		this.sensors = {};
+		for (i = 0; i < this.config.sensors.length; i++) {
+			this.sensors[this.config.sensors[i].mac] = {
+				temp: NaN,
+				humidity: NaN,
+				header: this.config.sensors[i].name
+			};
 		};
-		this.saunaSensor = {
-			MAC: this.config.saunaSensorMAC,
-			temp: NaN,
-			header: this.config.saunaSensorHeader
-		};
-		this.outdoorSensor = {
-			MAC: this.config.outdoorSensorMAC
-		};
+		this.queryURL = this.config.endpoint;
+		for (i = 0; i < this.sensors.length; i++){
+			if (i = 0){
+				this.queryURL = this.queryURL + "?filter=" + this.sensors[i].mac;
+			} else {
+				this.queryURL = this.queryURL + "&filter=" + this.sensors[i].mac;
+			}
+		}
+		this.scheduleUpdate(this.config.initialLoadDelay);
 	},
 	
 	// Generate dom object for module
@@ -40,35 +49,19 @@ Module.register("MMM-IndoorAndSaunaTemp", {
 		var wrapper = document.createElement("DIV");
 		wrapper.className = "large light sidebyside";
 		var degreeLabel = "°";
-		if (this.indoorSensor.temp != NaN) {
-			var indoorWrapper = document.createElement("DIV");
-			var indoorHeader = document.createElement("HEADER");
-			var indoorTempSpan = document.createElement("SPAN");
-			var indoorHeaderText = document.createTextNode(this.indoorSensor.header);
-			var indoorTemp = document.createTextNode(this.indoorSensor.temp.toFixed(1) + degreeLabel + "C" + " | " + this.indoorSensor.humidity.toFixed(0) + "%");
-			indoorTempSpan.className = "bright regular";
-			indoorTempSpan.appendChild(indoorTemp);
-			indoorHeader.appendChild(indoorHeaderText);
-			indoorWrapper.appendChild(indoorHeader);
-			indoorWrapper.appendChild(indoorTempSpan);
-			wrapper.appendChild(indoorWrapper);
-		}
-		if (this.saunaSensor.temp != NaN && this.saunaSensor.temp > this.config.saunaTempLimit) {
-			var saunaWrapper = document.createElement("DIV");
-                        var saunaHeader = document.createElement("HEADER");
-			var saunaTempSpan = document.createElement("SPAN");
-                        var saunaHeaderText = document.createTextNode(this.saunaSensor.header);
-			var saunaTemp = document.createTextNode(this.saunaSensor.temp.toFixed(1) + degreeLabel + "C");
-			if (this.saunaSensor.temp > this.config.saunaReadyLimit){
-				saunaTempSpan.className = "bright regular blinking";
-			} else {
-				saunaTempSpan.className = "bright regular";
-			}
-			saunaTempSpan.appendChild(saunaTemp);
-			saunaHeader.appendChild(saunaHeaderText);
-			saunaWrapper.appendChild(saunaHeader);
-			saunaWrapper.appendChild(saunaTempSpan);
-			wrapper.appendChild(saunaWrapper);
+		
+		for (var sensor in this.sensors){	
+			var sensorWrapper = document.createElement("DIV");
+			var sensorHeader = document.createElement("HEADER");
+			var sensorTempSpan = document.createElement("SPAN");
+			var sensorHeaderText = document.createTextNode(this.sensor[sensor].header);
+			var sensorTemp = document.createTextNode(this.sensor[sensor].temp.toFixed(1) + degreeLabel + "C" + " | " + this.sensor[sensor].humidity.toFixed(0) + "%");
+			sensorTempSpan.className = "bright regular";
+			sensorTempSpan.appendChild(sensorTemp);
+			sensorHeader.appendChild(sensorHeaderText);
+			sensorWrapper.appendChild(sensorHeader);
+			sensorWrapper.appendChild(sensorTempSpan);
+			wrapper.appendChild(sensorWrapper);
 		}
 		return wrapper;
 	},
@@ -79,34 +72,59 @@ Module.register("MMM-IndoorAndSaunaTemp", {
 			"MMM-IndoorAndSaunaTemp.css",
 		];
 	},
+	
+	// Update sensor data from rest gateway
+	updateSensors: function () {
+		var self = this;
+		var retry = true;
+		
+		if (this.config.endpoint === "") {
+			Log.error(self.name + ": Missing endpoint!");
+			return;
+		}
+		var dataRequest = new XMLHttpRequest();
+		dataRequest.open("GET", this.queryURL, true);
+		dataRequest.onreadystatechange = function () {
+			if (this.readyState === 4) {
+				if (this.status === 200) {
+					self.processSensorData(JSON.parse(this.response));
+				} else {
+					Log.error(self.name + ": Could not load sensor data.");
+				}
 
-	// Load translations files
-	getTranslations: function() {
-		return {
-			en: "translations/en.json",
-			es: "translations/es.json"
+				if (retry) {
+					self.scheduleUpdate(self.loaded ? -1 : self.config.retryDelay);
+				}
+			}
 		};
+		dataRequest.send();
+	},
+	
+	// Handle received data and update dom
+	processSensorData: function (data) {
+		for (i = 0; i < data.ruuvitags.length; i++){
+			this.sensors[data.ruuvitags[i].mac].temp = data.ruuvitags[i].temperature;
+			this.sensors[data.ruuvitags[i].mac].humidity = data.ruuvitags[i].humidity;
+		}
+		this.loaded = true;
+		this.updateDom(this.config.animationSpeed);
 	},
 	
 	// Override notification handler.
 	notificationReceived: function (notification, payload, sender) {
 		var self = this;
-		
-		if (notification == 'RUUVI_ENVIRONMENT_PACKET') {
-			var obj = JSON.parse(payload);
-
-			if (this.indoorSensor.MAC === obj.MAC) {
-				this.indoorSensor.temp = obj.temperature;
-				this.indoorSensor.humidity = obj.humidity;
-				self.updateDom();
-			}
-			else if (this.saunaSensor.MAC === obj.MAC) {
-				this.saunaSensor.temp = obj.temperature;
-				self.updateDom();
-			}
-			else if (this.outdoorSensor.MAC === obj.MAC) {
-				this.sendNotification("OUTDOOR_TEMPERATURE",obj.temperature);
-			}
+	},
+	
+	// Schedule next update
+	scheduleUpdate: function (delay) {
+		var nextLoad = this.config.updateInterval;
+		if (typeof delay !== "undefined" && delay >= 0) {
+			nextLoad = delay;
 		}
+
+		var self = this;
+		setTimeout(function () {
+			self.updateSensors();
+		}, nextLoad);
 	},
 });
